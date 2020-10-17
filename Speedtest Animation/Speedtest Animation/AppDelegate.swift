da8a0952d0edacc28b04c921da4b2f8fe29630aa
mike011 <mike_ch_1@hotmail.com>
2020-10-17 11:50:01 -0400

    Tab Bar View Controller Setup
    Getting the basic layout of app setup with the coloring of the buttons to match the spec.

--- Speedtest Animation/Speedtest Animation/AppDelegate.swift
+++ Speedtest Animation/Speedtest Animation/AppDelegate.swift
@@ -13,7 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let appearance = UITabBarAppearance()
+        // makes the background transparent
+        appearance.backgroundEffect = nil
+        UITabBar.appearance().standardAppearance = appearance
         return true
     }
 
